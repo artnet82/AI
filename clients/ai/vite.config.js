2aa32851436f4659cc657183b7a647991ca4f58a
Н Алексей <108236913+artnet82@users.noreply.github.com>
2023-12-19 03:13:40 +0300

    Update vite.config.js

--- clients/ai/vite.config.js
+++ clients/ai/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { nemtyrev_ai } from '@nemtyrev_ai/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
